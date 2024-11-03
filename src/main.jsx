ae9abdb7e8292967c8e150b28d5167c8ee05cff5
Rishan <98330109+NadimMahmudRishan@users.noreply.github.com>
2024-11-03 23:26:01 +0600

    Router not working.

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,25 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+import Root from "./components/root/Root";
+import Register from "./components/register/Register";
+import Login from "./components/login/Login";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root></Root>,
+
+    Children: [
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/register",
+        element: <Register></Register>,
+      },
+    ],
   },
 ]);
 createRoot(document.getElementById("root")).render(
